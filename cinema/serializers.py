9d9f667d13e4fed31ee17cdf1c993ff0ec4c180d
Andrii Shtaher <andriishtaher@gmail.com>
2025-05-11 20:04:10 +0200

    fix: MovieSerializer and GenreDetail (put)

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -8,9 +8,23 @@ class MovieSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=255)
     description = serializers.CharField()
     duration = serializers.IntegerField()
+    actors = serializers.PrimaryKeyRelatedField(
+        queryset=Actor.objects.all(),
+        many=True
+    )
+    genres = serializers.PrimaryKeyRelatedField(
+        queryset=Genre.objects.all(),
+        many=True
+    )
 
     def create(self, validated_data):
-        return Movie.objects.create(**validated_data)
+        actors_data = validated_data.pop("actors", [])
+        genres_data = validated_data.pop("genres", [])
+        movie = Movie.objects.create(**validated_data)
+        movie.actors.set(actors_data)
+        movie.genres.set(genres_data)
+
+        return movie
 
     def update(self, instance, validated_data):
         instance.title = validated_data.get("title", instance.title)
@@ -18,6 +32,14 @@ class MovieSerializer(serializers.Serializer):
             "description", instance.description
         )
         instance.duration = validated_data.get("duration", instance.duration)
+
+        actors_data = validated_data.get("actors", None)
+        if actors_data:
+            instance.actors.set(actors_data)
+
+        genres_data = validated_data.get("genres", None)
+        if genres_data:
+            instance.genres.set(genres_data)
 
         instance.save()
 
--- cinema/views.py
+++ cinema/views.py
@@ -46,6 +46,7 @@ class GenreDetail(APIView):
         genre = self.get_object(pk=pk)
         serializer = GenreSerializer(genre, data=request.data)
         if serializer.is_valid():
+            serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
